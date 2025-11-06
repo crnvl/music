e39a1a6dce557990e69ecdf343af4d14aade6c3e
luna <40737538+crnvl@users.noreply.github.com>
2025-11-06 21:30:45 +0100

    Update rave_set.js

--- rave_set.js
+++ rave_set.js
@@ -1,10 +1,8 @@
 await (async () => { (0, eval)(await (await fetch('https://raw.githubusercontent.com/switchangel/strudel-scripts/refs/heads/main/allscripts.js')).text()); })();
 
-const bpm = 128
-setCps(bpm /60 /4)
-
 samples('github:eddyflux/crate')
 samples('github:Nikeryms/Samples')
+samples('github:switchangel/breaks')
 
 
 
@@ -12,17 +10,25 @@ samples('github:Nikeryms/Samples')
 
 // OPTIONS
 const scale = "a:minor"
+const bpm = 130
+
+
+
+
+
+setCps(bpm /60 /4)
+
 
 
 
 
 
 // VOCALS
-$: s("vocals:1").fit().room(.25).pan(rand).n("-7").scale(scale).scrub(berlin.seg(8).rib("<9 9 8 1>",1 )).trancegate(1.5, 45, 1).o(2).gain(1.5)
+_$: s("vocals:1").fit().room(.25).pan(rand).n("-7").scale(scale).scrub(berlin.seg(8).rib("<9 9 8 1>",1 )).trancegate(1.5, 45, 1).o(2).gain(1.5)
 
 _$: s("wvocal:2/8").fit()
   .trancegate(.75, 0, 2)
-  .gain(.75)
+  .gain(1)
   .delay(.5).delaytime(bpm / 4)
   .room(1)
   .o(2)
@@ -40,28 +46,31 @@ _$: s("wvocal:2/8").fit()
 
 
 // DRUMS
-$: s("crate_bd:24!4").duckorbit("2:3:4").duckattack(bpm / 512).duckdepth(.85)
+_$: s("crate_bd:24!4").duckorbit("2").duckattack(bpm / 512).duckdepth(.75)
 
-$: s("crate_hh:1")
+_$: s("crate_hh:1")
   .gain(.75)
   .beat("1, 3, 5, 7", 8)
 
-$: s("hh!16")
+_$: s("hh!16")
   .gain(.75)
   .pan(rand)
   .o(3)
 
-$: s("crate_cp:2")
+_$: s("crate_cp:2")
   .room(.15)
   .delay(.25).delaytime(bpm / 100 / 8)
   .beat("<<4, 12>@3 <4, 12, 15>>", 16)
 
-$: s("crate_rim")
-  .gain(.75)
-  
+_$: s("crate_rim")
+  .gain(.75)  
   .trancegate(1.5, 45, 1)
   .o(2)
-  ._pianoroll()
+
+_$: s("breaks:4/2").fit()
+  .compressor(-10)
+  .gain(1)
+  ._scope()
 
 // ---------------------------------------------------------------------------------------------
 const basslines = [
@@ -75,7 +84,8 @@ const melodies = [
   "<3 2 [-5 -7 3 0 4] 0@2>",
   "0@2 <-7 [-5 -2]>@3 <0 -3 2 1>@3",
   "<-7 -5 -3 [-2 0]>",
-  "<12 12 10 10>@2 <7> <9 3>"
+  "<12 12 10 10>@2 <7> <9 3>",
+  "<7 4 6 2>*2@3 <4 6 2 7>*2".add("0, -7")
 ]
 
 const arps = [
@@ -85,9 +95,18 @@ const arps = [
 
 // ---------------------------------------------------------------------------------------------
 
-const bassline = basslines[1]
-const melody   = melodies[3]
-const arp      =  arps[1]
+const bassline = basslines[2]
+const melody   = melodies[4]
+const arp      = melodies[4]
+
+
+
+
+
+
+
+
+
 
 
 
@@ -104,13 +123,15 @@ const arp      =  arps[1]
 // BASS SECTION
 
 // - - - - MID BASS
-$: s("supersaw")
+_$: s("supersaw")
   //.fm(.5).fmwave("brown")
   .sustain(slider(0.508,0,1))
   .decay(slider(0.568,0,1))
-  .release(slider(0.172))
-  
-  .rlpf(slider(0.363))
+  .release(slider(0.446))
+
+  .hpf(350)
+  //.rlpf(slider(0.625))
+  .rlpf("<.35 .45 .5 .65>")
   .compressor(10)
   .gain(1.35)
   
@@ -120,20 +141,18 @@ $: s("supersaw")
     .sub("14, 21")
   ).scale(scale)
   .o(2)
-  ._pianoroll()
 // - - - - SUB BASS
 _$: s("sine")
   .fm(.5).fmwave("saw")
   .distort("3:3")
   .gain(.5)
-  .rlpf(slider(0.386))
+  .rlpf(slider(0.302))
   .n(
     bassline
-    .sub("14")
+    .sub(21)
   )
   .scale(scale)
   .o(2)
-  ._scope()
 
 // ---------------------------------------------------------------------------------------------
 
@@ -144,18 +163,19 @@ _$: s("sine")
 // SYNTH SECTION
 
 // - - - - LEAD SYNTH
-$: s("sawtooth")
+_$: s("supersaw")
   .fm(.5).fmwave("brown")
-  .sustain(slider(0.728,0,1))
-  .decay(slider(0.285,0,1))
+  .sustain(slider(0.673,0,1))
+  .decay(slider(0.254,0,1))
   .release("<.2 .4 .6 .8>")
 
   .hpf(500)
-  .rlpf(slider(0.324))
+  .rlpf(slider(0.466))
   .delay(.75)
   .delaytime(bpm / 4)
   .room(1)
   .compressor(10)
+  .pan(rand)
   
   .trancegate(1.5, 90, 1)
   .n(
@@ -163,7 +183,6 @@ $: s("sawtooth")
     .add(0)
   ).scale(scale)
   .o(4)
-  ._pianoroll()
 
 
 
@@ -177,6 +196,5 @@ _$: s("sine!16")
     arp
      .add(14)
   ).scale(scale)
-  ._pianoroll()
 
 // ---------------------------------------------------------------------------------------------
