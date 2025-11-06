3046530ec2b00fe0d8c507ce2399f45b9c382935
luna <40737538+crnvl@users.noreply.github.com>
2025-11-06 19:53:52 +0100

    Update rave_set.js

--- rave_set.js
+++ rave_set.js
@@ -10,8 +10,6 @@ samples('github:Nikeryms/Samples')
 
 
 
-
-
 // OPTIONS
 const scale = "a:minor"
 
@@ -20,10 +18,16 @@ const scale = "a:minor"
 
 
 // VOCALS
-_$: s("vocals:1").fit().room(.25).pan(rand).n("-7").scale(scale).scrub(berlin.seg(8).rib("<9 9 8 1>",1 )).trancegate(1.5, 45, 1).o(2).gain(1.5)
+$: s("vocals:1").fit().room(.25).pan(rand).n("-7").scale(scale).scrub(berlin.seg(8).rib("<9 9 8 1>",1 )).trancegate(1.5, 45, 1).o(2).gain(1.5)
 
 _$: s("wvocal:2/8").fit()
-  //.trancegate(1.5, 45, 1)
+  .trancegate(.75, 0, 2)
+  .gain(.75)
+  .delay(.5).delaytime(bpm / 4)
+  .room(1)
+  .o(2)
+
+_$: s("wvocal:2/8").fit()
   .gain(1)
   .delay(.5).delaytime(bpm / 4)
   .room(1)
@@ -36,18 +40,18 @@ _$: s("wvocal:2/8").fit()
 
 
 // DRUMS
-_$: s("crate_bd:24!4").duckorbit("2:3:4").duckattack(bpm / 512).duckdepth(.85)
+$: s("crate_bd:24!4").duckorbit("2:3:4").duckattack(bpm / 512).duckdepth(.85)
 
-_$: s("crate_hh:1")
+$: s("crate_hh:1")
   .gain(.75)
   .beat("1, 3, 5, 7", 8)
 
-_$: s("hh!16")
+$: s("hh!16")
   .gain(.75)
   .pan(rand)
   .o(3)
 
-_$: s("crate_cp:2")
+$: s("crate_cp:2")
   .room(.15)
   .delay(.25).delaytime(bpm / 100 / 8)
   .beat("<<4, 12>@3 <4, 12, 15>>", 16)
@@ -60,13 +64,30 @@ $: s("crate_rim")
   ._pianoroll()
 
 // ---------------------------------------------------------------------------------------------
+const basslines = [
+  "<3@3 4 5 @3 6>*2",
+  "<0@6 5 6>",
+  "<0 2 3 5>",
+  "<3 5 7 9>@3 <10 12 14 16>",
+]
 
+const melodies = [
+  "<3 2 [-5 -7 3 0 4] 0@2>",
+  "0@2 <-7 [-5 -2]>@3 <0 -3 2 1>@3",
+  "<-7 -5 -3 [-2 0]>",
+  "<12 12 10 10>@2 <7> <9 3>"
+]
 
+const arps = [
+  "<14 16 12 10>*8".sub(7),
+  "<0@4 5@2 6@2>*8".sub(7),
+]
 
+// ---------------------------------------------------------------------------------------------
 
-const bassline = "<3 5 7 9>@3 <10 12 14 16>"
-const melody   = "<12 12 10 10>@2 <7> <9 3>"
-const arp      =  "<14 16 12 10>*8".sub(7)
+const bassline = basslines[1]
+const melody   = melodies[3]
+const arp      =  arps[1]
 
 
 
@@ -83,13 +104,13 @@ const arp      =  "<14 16 12 10>*8".sub(7)
 // BASS SECTION
 
 // - - - - MID BASS
-_$: s("supersaw")
+$: s("supersaw")
   //.fm(.5).fmwave("brown")
   .sustain(slider(0.508,0,1))
   .decay(slider(0.568,0,1))
-  .release(slider(0.576))
+  .release(slider(0.172))
   
-  .rlpf(slider(0.382))
+  .rlpf(slider(0.363))
   .compressor(10)
   .gain(1.35)
   
@@ -123,14 +144,14 @@ _$: s("sine")
 // SYNTH SECTION
 
 // - - - - LEAD SYNTH
-_$: s("sawtooth")
+$: s("sawtooth")
   .fm(.5).fmwave("brown")
   .sustain(slider(0.728,0,1))
   .decay(slider(0.285,0,1))
   .release("<.2 .4 .6 .8>")
 
-  .hpf(slider(1060,0,20000))
-  .rlpf(slider(0.385))
+  .hpf(500)
+  .rlpf(slider(0.324))
   .delay(.75)
   .delaytime(bpm / 4)
   .room(1)
